d5f09fe62ab81ac1f2fdf98dc4021f6b713e0d1b
Avshek <shakyavi@gmail.com>
2022-05-30 07:57:06 +0545

    front end setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 import withSocket from "./withSocket"
+import {Row,Col,Container, Card, Form, Button, ListGroup} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App({socketListen,socketSend}) {
+function App({socketListen, socketSend}) {
 
-  socketListen("consumerResponse", (response) => {
-    console.log(`\nsocket listening to consumerResponse`)
-    console.log(response)
-  })
+    socketListen("consumerResponse", (response) => {
+        console.log(`\nsocket listening to consumerResponse`)
+        console.log(response)
+    })
 
-console.log(`emitting socket 'messageFromApp' `)
-  socketSend("messageFromApp", {name:"I am connected"})
+    console.log(`emitting socket 'messageFromApp' `)
+    socketSend("messageFromApp", {name: "I am connected"})
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <Container>
+                        <Form>
+                    <Row>
+                    <Col>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Request Rate</Form.Label>
+                                <Form.Control type="text" placeholder="Message rate per second"/>
+                            </Form.Group>
+                    </Col>
+                    <Col>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Simulation Time</Form.Label>
+                                <Form.Control type="text" placeholder="Simulation time in seconds"/>
+                            </Form.Group>
+                    </Col>
+                    <Col>
+                            <Button variant="primary" type="submit">
+                                Start simulation
+                            </Button>
+                    </Col>
+                    </Row>
+                        </Form>
+
+                    <Row>
+                            <ListGroup>
+                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            </ListGroup>
+                    </Row>
+                </Container>
+            </header>
+
+
+        </div>
+    );
 }
 
 export default withSocket(App);
